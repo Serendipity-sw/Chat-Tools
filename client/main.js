7f34e520f461bd9bc50dedb38c5872bf4287059f
sw.gloomysw@gmail.com <sw.gloomysw@gmail.com>
2020-07-13 22:17:55 +0800

    :art: 项目初始化

---
+++ client/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  // 加载index.html文件
+  win.loadURL('http://localhost:8080/');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
