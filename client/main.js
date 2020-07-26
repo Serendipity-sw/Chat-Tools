34393c43c99c6347fe9a53ecb064527f726760fc
sw.gloomysw@gmail.com <sw.gloomysw@gmail.com>
2020-07-26 23:38:07 +0800

    :art: 修正菜单,调整调试模式

--- client/main.js
+++ client/main.js
@@ -13,27 +13,26 @@ const createWindow = () => {
   // 加载index.html文件
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL('http://localhost:8090/index.html');
-  }else{
-    mainWindow.loadFile(process.resourcesPath+'/index.html');
+  } else {
+    mainWindow.loadFile(process.resourcesPath + '/index.html');
   }
-  mainWindow.webContents.openDevTools();
 };
 // 菜单
-// const menuList = [
-//   {
-//     label: 'dev',
-//     submenu: [
-//       {
-//         label: '调试',
-//         click: () => {
-//           mainWindow.webContents.openDevTools();
-//         }
-//       }
-//     ]
-//   }
-// ];
-// const appMenu = Menu.buildFromTemplate(menuList);
-// Menu.setApplicationMenu(appMenu);
+const menuList = [
+  {
+    label: 'dev',
+    submenu: [
+      {
+        label: '调试',
+        click: () => {
+          mainWindow.webContents.openDevTools();
+        }
+      }
+    ]
+  }
+];
+const appMenu = Menu.buildFromTemplate(menuList);
+Menu.setApplicationMenu(appMenu);
 app.whenReady().then(createWindow);
 
 
