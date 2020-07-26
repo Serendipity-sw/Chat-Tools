89c28379e1506e43db3c7806c989517cb394663b
sw.gloomysw@gmail.com <sw.gloomysw@gmail.com>
2020-07-26 22:34:35 +0800

    :art: 增加调试模式

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
-const { app, BrowserWindow } = require('electron');
-
-function createWindow () {
+const { app, BrowserWindow, Menu } = require('electron');
+let mainWindow;
+const createWindow = () => {
   // 创建浏览器窗口
-  let win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1200,
     height: 700,
     webPreferences: {
@@ -12,8 +12,24 @@ function createWindow () {
   });
   // 加载index.html文件
   // win.loadFile(process.resourcesPath+'/index.html');
-  win.loadURL('http://localhost:8090/index.html');
-  win.webContents.openDevTools();
-}
-
+  mainWindow.loadURL('http://localhost:8090/index.html');
+};
+// 菜单
+const menuList = [
+  {
+    label: 'dev',
+    submenu: [
+      {
+        label: '调试',
+        click: () => {
+          mainWindow.webContents.openDevTools();
+        }
+      }
+    ]
+  }
+];
+const appMenu = Menu.buildFromTemplate(menuList);
+Menu.setApplicationMenu(appMenu);
 app.whenReady().then(createWindow);
+
+
