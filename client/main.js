f271eb164d0a8f27030b185fab8a4ffe87a015a0
gloomysw <sw.gloomysw@gmail.com>
2021-05-02 14:51:06 +0800

    🙈 删除无效代码

--- client/main.js
+++ client/main.js
@@ -19,20 +19,20 @@ const createWindow = () => {
   if (process.env.NODE_ENV === 'development') {
     mainWindow.webContents.openDevTools()
     mainWindow.loadURL('http://localhost:8080/index.html');
-    if (process.platform === 'win32') {
-      // windows 系统
-      exec(path.join(__dirname, '../service/service.exe'))
-    } else {
-      exec(path.join(__dirname, '../service/service'))
-    }
+    // if (process.platform === 'win32') {
+    //   // windows 系统
+    //   exec(path.join(__dirname, '../service/service.exe'))
+    // } else {
+    //   exec(path.join(__dirname, '../service/service'))
+    // }
   } else {
     mainWindow.loadFile(process.resourcesPath + '/index.html');
-    if (process.platform === 'win32') {
-      // windows 系统
-      exec(process.resourcesPath + '/service.exe')
-    } else {
-      exec(process.resourcesPath + '/service')
-    }
+    // if (process.platform === 'win32') {
+    //   // windows 系统
+    //   exec(process.resourcesPath + '/service.exe')
+    // } else {
+    //   exec(process.resourcesPath + '/service')
+    // }
   }
   // 注册快捷键
   globalShortcut.register('f12', () => {
