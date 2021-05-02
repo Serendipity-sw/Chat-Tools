f42af387ad9599321c46ab1b0b40d057e195bc01
gloomysw <sw.gloomysw@gmail.com>
2021-05-02 14:50:03 +0800

    🚧 注册F12快捷键

--- client/main.js
+++ client/main.js
@@ -1,43 +1,47 @@
-const {app, BrowserWindow, Menu, ipcMain} = require('electron');
+const {app, BrowserWindow, globalShortcut, Menu, ipcMain} = require('electron');
 const wallpaper = require('wallpaper')
 const exec = require('child_process').execFile
 const path = require('path');
 
 let mainWindow;
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 700,
-        webPreferences: {
-            webSecurity: false,
-            nodeIntegration: false,
-            contextIsolation: true,
-            enableRemoteModule: false,
-            preload: path.join(__dirname, 'js/preload.js')
-        }
-    });
-    mainWindow.webContents.openDevTools()
-    if (process.env.NODE_ENV === 'development') {
-        mainWindow.loadURL('http://localhost:8080/index.html');
-        if (process.platform === 'win32') {
-            // windows 系统
-            exec(path.join(__dirname, '../service/service.exe'))
-        } else {
-            exec(path.join(__dirname, '../service/service'))
-        }
-    } else {
-        mainWindow.loadFile(process.resourcesPath + '/index.html');
-        if (process.platform === 'win32') {
-            // windows 系统
-            exec(process.resourcesPath + '/service.exe')
-        } else {
-            exec(process.resourcesPath + '/service')
-        }
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    webPreferences: {
+      webSecurity: false,
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'js/preload.js')
     }
+  });
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools()
+    mainWindow.loadURL('http://localhost:8080/index.html');
+    if (process.platform === 'win32') {
+      // windows 系统
+      exec(path.join(__dirname, '../service/service.exe'))
+    } else {
+      exec(path.join(__dirname, '../service/service'))
+    }
+  } else {
+    mainWindow.loadFile(process.resourcesPath + '/index.html');
+    if (process.platform === 'win32') {
+      // windows 系统
+      exec(process.resourcesPath + '/service.exe')
+    } else {
+      exec(process.resourcesPath + '/service')
+    }
+  }
+  // 注册快捷键
+  globalShortcut.register('f12', () => {
+    mainWindow.openDevTools(); // 打开开发者工具
+  });
 
 };
 Menu.setApplicationMenu(null);
 app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
-    app.exit(0);
+  app.exit(0);
 })
